a78c998f447b0658a627c1b1fed3827377d1881f
Phil E. Taylor <phil@phil-taylor.com>
2023-04-21 13:08:52 +0100

    Update example.php

--- config/packages/example.php
+++ config/packages/example.php
@@ -1,12 +1,6 @@
 <?php
 
 declare(strict_types=1);
-/*
- * @copyright  Copyright (C) 2017,2018,2019,2020,2021,2022,2023 Blue Flame Digital Solutions Limited / Phil Taylor. All rights reserved.
- * @author     Phil Taylor <phil@phil-taylor.com>
- * @see        https://github.com/PhilETaylor/manage.mysites.guru
- * @license    Commercial License - Not For Distribution.
- */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
